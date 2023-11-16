cf2137cd412370c6681660ac9775b3d8a6589cc5
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-11-16 21:49:22 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,17 @@
 import { Layout } from "./Layout/Layout";
 import { TaskList } from "./TasksList/TasksList";
 import { AppBar } from "./AppBar/AppBar";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { TaskForm } from "./TaskForm/TaskForm";
 import { useEffect } from "react";
 import { fetchTasks } from "redux/operations";
+import { getError, getIsLoading } from "redux/selectors";
 
 export const App = () => {
 
   const dispatch = useDispatch();
+  const isLoading = useSelector(getIsLoading);
+  const error = useSelector(getError);
 
 useEffect(() => {
   dispatch(fetchTasks());
@@ -18,6 +21,7 @@ useEffect(() => {
     <Layout>
       <AppBar />
       <TaskForm /> 
+      {isLoading && !error && <b>Request in progress...</b>}
       <TaskList />
     </Layout>
   );
