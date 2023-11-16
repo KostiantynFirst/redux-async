9d07509df159dfa444ba54833509aad4a91e875d
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-11-16 19:56:22 +0200

    add async elements

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,18 @@
 import { Layout } from "./Layout/Layout";
 import { TaskList } from "./TasksList/TasksList";
 import { AppBar } from "./AppBar/AppBar";
+import { useDispatch } from "react-redux";
 import { TaskForm } from "./TaskForm/TaskForm";
 
-const defaultTasks = [
-  { id: 0, text: 'Learn HTML and CSS', completed: true },
-  { id: 1, text: 'Get good at JavaScript', completed: true },
-  { id: 2, text: 'Master React', completed: false },
-  { id: 3, text: 'Discover Redux', completed: false },
-  { id: 4, text: 'Build amazing apps', completed: false },
-];
-
 export const App = () => {
+
+const dispatch = useDispatch();
+
   return (
     <Layout>
       <AppBar />
       <TaskForm /> 
-      <TaskList tasks={defaultTasks} />
+      <TaskList />
     </Layout>
   );
 };
--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,2 +1,19 @@
 //https://6553de625449cfda0f2f42e0.mockapi.io 
 
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://6553de625449cfda0f2f42e0.mockapi.io';
+
+export const fetchContacts = createAsyncThunk('contacts/fetchAll', async () => {
+    try {
+        const res = await axios.get('/contacts');
+        return res.data;
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+//pending -> 'contacts/fetchAll/pending';
+//fulfield -> 'contacts/fetchAll/fulfield';
+//rejected -> 'contacts/fetchAll/rejected';
