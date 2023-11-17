1c5b2422ae73af90f419ed94a9a1cf2c57dc2257
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-11-17 15:37:33 +0200

    Update tasksSlice.js

--- src/redux/tasksSlice.js
+++ src/redux/tasksSlice.js
@@ -17,8 +17,6 @@ const handleRejected = (state, action) => {
         isLoading: false,
         error: null,
     },
-    reducers: {
-    },
     extraReducers: {
         [fetchTasks.pending]: handlePending,
         [fetchTasks.fulfilled](state, action) {
