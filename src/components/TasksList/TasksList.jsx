0cb5d8e818cfc487d9cf8a13d815393eeccc7262
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-11-19 13:35:46 +0200

    selectors add

--- src/components/TasksList/TasksList.jsx
+++ src/components/TasksList/TasksList.jsx
@@ -1,10 +1,12 @@
+import { useSelector } from "react-redux";
 import { StyledUl, TaskListItem } from "./TaskLists.styled"
-import { useVisibleTasks } from "redux/hooks/useVisibleTasks";
+import { selectVisibleTasks } from "redux/selectors";
 import { Task } from "components/Task/Task";
+
 
 export const TaskList = () => {
 
-    const tasks = useVisibleTasks();
+    const tasks = useSelector(selectVisibleTasks);
 
     return (
         <StyledUl>
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,20 +1,36 @@
+import { createSelector } from "@reduxjs/toolkit";
+import { statusFilters } from "./constants";
+
 export const selectTasks = state => state.tasks.items;
 export const selectIsLoading = state => state.tasks.isLoading;
 export const selectError = state => state.tasks.error;
 export const selectStatusFilter = state => state.filters.status;
 
-export const selectTaskCount = state => {
-    const tasks = selectTasks(state);
-
+export const selectTaskCount = createSelector([selectTasks], (tasks) => {
     return tasks.reduce(
-    (count, task) => {
-        if(task.completed) {
-            count.completed += 1;
-        } else {
-            count.active += 1;
-        }
-        return count;
-    },
-    { active: 0, completed: 0}
-    );
-}
+        (count, task) => {
+            if(task.completed) {
+                count.completed += 1;
+            } else {
+                count.active += 1;
+            }
+            return count;
+        },
+        { active: 0, completed: 0}
+        );
+});
+
+export const selectVisibleTasks = state => {
+
+    const tasks = selectTasks(state);
+    const filter = selectStatusFilter(state);
+
+    switch (filter) {
+        case statusFilters.active:
+            return tasks.filter(task => !task.completed);
+        case statusFilters.completed:
+            return tasks.filter(task => task.completed);
+        default:
+            return tasks;
+    }
+};
