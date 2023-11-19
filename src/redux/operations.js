1f36d90d47539fd705d0482110870636bd8fbae9
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-11-19 22:04:36 +0200

    j

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,11 +1,6 @@
-//https://6553de625449cfda0f2f42e0.mockapi.io 
-
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-// https://62584f320c918296a49543e7.mockapi.io
-
-// https://655663ce84b36e3a431fb080.mockapi.io
 axios.defaults.baseURL = 'https://655663ce84b36e3a431fb080.mockapi.io';
 
 export const fetchTasks = createAsyncThunk(
